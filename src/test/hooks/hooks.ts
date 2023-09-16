08f512c8a1789893fba84d0604c1e088be1a3480
abuzova <alla.abuzova@gmail.com>
2023-09-16 16:47:04 +0200

    adding hooks.ts and pageFixture.ts

---
+++ src/test/hooks/hooks.ts
@@ -0,0 +1,18 @@
+import {BeforeAll, AfterAll, Before, After} from  "@cucumber/cucumber";
+import { Browser, chromium } from "@playwright/test";
+import { pageFixture } from "./pageFixture";
+
+let browser: Browser;
+
+Before(async function() {
+
+    browser = await chromium.launch({ headless: false });    
+    const page = await browser.newPage();
+    pageFixture.page = page;
+
+})
+
+After(async function (){
+    await pageFixture.page.close();
+    await browser.close();
+})
--- src/test/steps/login.ts
+++ src/test/steps/login.ts
@@ -1,46 +1,48 @@
 const { Given, When, Then, setDefaultTimeout } = require('@cucumber/cucumber');
 //import footerPage from "../pages/FooterPage.spec";
-import { expect, chromium, Page, Browser } from "@playwright/test";
+import { Browser, expect } from "@playwright/test";
+import { pageFixture } from "../hooks/pageFixture";
 
 let browser: Browser;
-let page: Page;
-setDefaultTimeout(60 * 1000);
 
-Given('User navigate to the Log in page', async () => {
-    browser = await chromium.launch({ headless: false });    
-    page = await browser.newPage();
-    page.goto('https://a.cms.omniupdate.com/11/#qa/fari-test/gallena');
+setDefaultTimeout(60 * 1000 * 2);
+
+Given('User navigate to the Log in page', async () => {  
+   
+    await pageFixture.page.goto('https://a.cms.omniupdate.com/11/#qa/fari-test/gallena');
    
   });
 
 Given('User enter username', async () => {
-    const name = await page.locator('form[id="form-login"]').getByRole('textbox', {name: 'username'});
+    const name = await pageFixture.page.locator('form[id="form-login"]').getByRole('textbox', {name: 'username'});
     await name.fill('example value');
     await expect(name).toHaveValue('example value');       
 });
 
 Given('User enter password', async () => {    
-    const password = await page.locator('form[id="form-login"]').getByRole('textbox', {name: 'password'});
+    const password = await pageFixture.page.locator('form[id="form-login"]').getByRole('textbox', {name: 'password'});
     await password.fill('password value');
     //await expect(password).toHaveValue('password value');    
 });
 
 When('User click on the "Login" button', async () => {
-    await page.locator('button[type="submit"]', { hasText: 'LOGIN' }).click();
-    await page.waitForURL('**/gallena');
+    await pageFixture.page.locator('button[type="submit"]', { hasText: 'LOGIN' }).click();
+    await pageFixture.page.waitForURL('**/gallena');
 });
 
 Then("User log in successfully",  async () => {
     //const successMassage = await page.locator('form[id="form-login"] div[class="login-message alert alert-error"]');
     //await expect(successMassage).toHaveText('The login information provided is invalid.');
-    await browser.close();    
+    //await browser.close();
+    
 });
 
 Then('Login should fail', async () => {
-    const failMassage = await page.locator('form[id="form-login"] div[class="login-message alert alert-error"]');
-    await expect(page.locator('form[id="form-login"] div[class="login-message alert alert-error"]')).toHaveText('The login information provided is invalid.');
-    await expect(failMassage).toBeVisible();   
-    await browser.close();   
+    const failMassage = await pageFixture.page.locator('form[id="form-login"] div[class="login-message alert alert-error"]');
+    await expect(failMassage).toHaveText('The login information provided is invalid.');
+    await expect(failMassage).toBeVisible();
+    //await browser.close();   
+   
 }); 
 
 
