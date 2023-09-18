c322d0bfa94a5a85c348ac28a03d03eb017545a3
abuzova <alla.abuzova@gmail.com>
2023-09-18 21:27:41 +0200

    adding chenges gith actions

--- src/helper/report.ts
+++ src/helper/report.ts
@@ -1,7 +1,7 @@
 const report = require("multiple-cucumber-html-reporter");
 
 report.generate({
-  jsonDir: "test-results",
+  jsonDir: "./test-results",
   reportPath: "./",
   metadata: {
     browser: {
